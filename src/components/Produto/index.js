d0cbe8be45109fe6adbe784c36bd2c116f88d8bb
Jennifer <jennifertorchelsen@gmail.com>
2023-02-13 14:21:06 -0300

    primeiro commit

---
+++ src/components/Produto/index.js
@@ -0,0 +1,42 @@
+import { Container } from './styles';
+import { memo, useContext } from 'react';
+import { IconButton } from '@material-ui/core';
+import AddIcon from '@material-ui/icons/Add';
+import RemoveIcon from '@material-ui/icons/Remove';
+import { CarrinhoContext } from 'common/context/Carrinho';
+
+
+function Produto({
+  nome,
+  foto,
+  id,
+  valor,
+  unidade
+}) {
+  const {carrinho, setCarrinho} = useContext(CarrinhoContext);
+  return (
+      <Container>
+        <div>
+          <img
+            src={`/assets/${foto}.png`}
+            alt={`foto de ${nome}`}
+          />
+          <p>
+            {nome} - R$ {valor?.toFixed(2)} <span>Kg</span>
+          </p>
+        </div>
+        <div>
+          <IconButton
+            color="secondary"
+          >
+            <RemoveIcon />
+          </IconButton>
+          <IconButton>
+            <AddIcon />
+          </IconButton>
+        </div>
+      </Container>
+  )
+}
+
+export default memo(Produto)

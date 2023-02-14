34e80ada3f8385e307611b7acf63682a4198e29b
Jennifer <jennifertorchelsen@gmail.com>
2023-02-14 17:55:57 -0300

    Adicionando no carrinho

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,61 +1,44 @@
-import { Button } from '@material-ui/core';
-import {
-  Container,
-  Titulo,
-  InputContainer
-} from './styles';
-import {
-  Input,
-  InputLabel,
-  InputAdornment 
-} from '@material-ui/core';
-import {useHistory} from 'react-router-dom'
-import { UsuarioContext } from 'common/context/Usuario';
-import { useContext } from 'react';
+import { Button } from "@material-ui/core";
+import { Container, Titulo, InputContainer } from "./styles";
+import { Input, InputLabel, InputAdornment } from "@material-ui/core";
+import { useHistory } from "react-router-dom";
+import { UsuarioContext } from "common/context/Usuario";
+import { useContext } from "react";
 
 function Login() {
   const history = useHistory();
-  const {nome, setNome, saldo, setSaldo} = useContext(UsuarioContext);
+  const { nome, setNome, saldo, setSaldo } = useContext(UsuarioContext);
   return (
     <Container>
-              <Titulo>
-                Insira o seu nome
-              </Titulo>
+      <Titulo>Insira o seu nome</Titulo>
 
-              <InputContainer>
-                <InputLabel>
-                  Nome
-                </InputLabel>
-                <Input
-                value={nome}
-                onChange={(event)=> setNome(event.target.value)}
-                  type="text"
-                />
-              </InputContainer>
-              <InputContainer>
-                <InputLabel>
-                  Saldo
-                </InputLabel>
-                <Input
-                type="number"
-                value={saldo}
-                onChange={(event) => setSaldo(event.target.value)}
-                startAdornment={
-                  <InputAdornment position="start">
-                    R$
-                  </InputAdornment>
-                }
-              />
-              </InputContainer>
-              <Button
-                variant="contained"
-                color="primary"
-                onClick={() => history.push('/feira')}
-              >
-                Avançar
-              </Button>
+      <InputContainer>
+        <InputLabel>Nome</InputLabel>
+        <Input
+          value={nome}
+          onChange={(event) => setNome(event.target.value)}
+          type="text"
+        />
+      </InputContainer>
+      <InputContainer>
+        <InputLabel>Saldo</InputLabel>
+        <Input
+          type="number"
+          value={saldo}
+          onChange={(event) => setSaldo(event.target.value)}
+          startAdornment={<InputAdornment position="start">R$</InputAdornment>}
+        />
+      </InputContainer>
+      <Button
+        variant="contained"
+        color="primary"
+        disabled={nome.lenght < 4}
+        onClick={() => history.push("/feira")}
+      >
+        Avançar
+      </Button>
     </Container>
-  )
-};
+  );
+}
 
 export default Login;
